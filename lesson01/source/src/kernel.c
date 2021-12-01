87a2462efeee9e411166a40b77bd1e9c50f9b703
Zhao233 <953071759@qq.com>
2021-12-01 04:43:07 -0800

    完成多核启动练习

--- lesson01/source/src/kernel.c
+++ lesson01/source/src/kernel.c
@@ -1,11 +1,18 @@
 #include "mini_uart.h"
 
-void kernel_main(void){
+void kernel_main(unsigned long long processor_id){
     
-    uart_init();
-    uart_send_string("Hello world! zx's kernel!");
+    if(processor_id == 0){
+        uart_init();
+        uart_send_string("Hello world! zx's kernel! \n");
 
+    }
+
+    uart_send_string("it's proceesor: " + processor_id);
+    uart_send_string("\n\n");
+
+    // hang all processor
     while(1){
-        uart_send(uart_recv());
+        //uart_send(uart_recv());
     }
 }
