76bc87922a397516acd4e66dbcf922d45cc46928
Zhao233 <953071759@qq.com>
2021-12-17 08:32:18 -0800

    添加中断处理入口函数handle_irq

--- lesson03/source/src/irq.c
+++ lesson03/source/src/irq.c
@@ -1,3 +1,9 @@
+#include "utils.h"
+#include "printf.h"
+#include "timer.h"
+#include "entry.h"
+#include "peripherals/irq.h"
+
 const char *entry_error_message[] = {
     "SYNC_INVALID_EL1t",
     "IRQ_INVALID_EL1t",
@@ -29,4 +35,19 @@ void show_invalid_entry_message(int type, unsigned long esr, unsigned long addre
 void enable_interrupt_controller()
 {
     put32(ENABLE_IRQS_1, SYSTEM_TIMER_IRQ_1);
+}
+
+// the entry of the irq
+void handle_irq(void)
+{
+    unsigned int irq = get32(IRQ_PENDING_1);
+
+    switch(irq){
+        case (SYSTEM_TIMER_IRQ_1):
+            handle_timer_irq();
+            break;
+
+        default:
+            printf("Unknown pending irq: %x\r\n", irq);
+    }
 }
