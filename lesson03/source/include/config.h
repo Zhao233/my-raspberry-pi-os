9a87ed860e81597b79823059d872235f7791481d
Zhao233 <953071759@qq.com>
2022-01-04 10:45:45 -0800

    配置头文件，决定哪些代码块会被编译

---
+++ lesson03/source/include/config.h
@@ -0,0 +1,7 @@
+/*
+ * timer, choose system timer or local timer to generate the timer interrupt. 
+ * in practice, you can choose only one option(you have to annotate other timer)
+ */ 
+
+//#define SYSTEM_TIMER
+#define LOCAL_TIMER
