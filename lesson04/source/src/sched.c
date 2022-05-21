44e720753119e08c1f704be54be7adab115f6f69
Zhao233 <953071759@qq.com>
2022-05-21 04:40:27 -0700

    set nr_tasks = 1

--- lesson04/source/src/sched.c
+++ lesson04/source/src/sched.c
@@ -7,6 +7,16 @@ static task_struct init_task = &INIT_TASK;
 struct task_struct *current = &init_task;
 struct task_struct *task[NR_TASKS] = {&init_task, };
 
-int nr_tasks;
+int nr_tasks = 1;
 
-//
+void preempt_disable(void)
+{
+    current -> preempt_counter++;
+}
+
+void preempt_enable(void)
+{
+    current -> preempt_counter--;
+}
+
+void timer_tick(){}
