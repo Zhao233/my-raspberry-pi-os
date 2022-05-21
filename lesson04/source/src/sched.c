6a88e23a7fbe13f9b13cbdb8a026ac79279140b7
Zhao233 <953071759@qq.com>
2022-05-21 08:11:23 -0700

    add main schedule function

--- lesson04/source/src/sched.c
+++ lesson04/source/src/sched.c
@@ -20,6 +20,49 @@ void preempt_enable(void)
 }
 
 /**
+ * schedule main function, select the next task that needs to run. preempt must be disbale during schedule
+ * 1. iterate all task
+ * */
+static void _schedule()
+{
+void _schedule(void)
+{
+	preempt_disable();
+	int next,c;
+	struct task_struct * p;
+	while (1) {
+		c = -1;
+		next = 0;
+		for (int i = 0; i < NR_TASKS; i++){
+			p = task[i];
+			if (p && p->state == TASK_RUNNING && p->counter > c) {
+				c = p->counter;
+				next = i;
+			}
+		}
+		if (c) {
+			break;
+		}
+		for (int i = 0; i < NR_TASKS; i++) {
+			p = task[i];
+			if (p) {
+				p->counter = (p->counter >> 1) + p->priority;
+			}
+		}
+	}
+	switch_to(task[next]);
+	preempt_enable();
+}
+}
+
+void schedule(void)
+{
+    current -> counter = 0;
+
+    _schedule();
+}
+
+/**
  * pre-process before cpu_switch_to
  * set current = next
  * switch cpu context
@@ -35,6 +78,10 @@ void switch_to(struct task_struct *next)
    current = next;
    
    cpu_switch_to(prev, next);
+}
+
+void schedule_tail(void) {
+	preempt_enable();
 }
 
 
