45bf95b0b85a80bf662f5cdfe6e21e51e03c2dee
Zhao233 <953071759@qq.com>
2022-06-11 06:56:09 -0700

    add container of header file

---
+++ lesson04/source/include/container_of.h
@@ -0,0 +1,14 @@
+#ifndef _CONTAINER_OF_H
+
+#define _CONTAINER_OF_H
+
+#define offsetof(type,member) ((size_t)&(type *)0 -> member)
+
+#define container_of(ptr,type,member) ({ \
+                const typeof((type *)0 -> member) *__mptr = (ptr); \
+                (type *) ((char*)__mptr - offsetof(type, member)); \
+                })
+
+#endif
+
+
