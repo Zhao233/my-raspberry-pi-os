9dabc8520f0f8821b8b3c7c35a5f09904b3d07a4
Zhao233 <953071759@qq.com>
2022-06-12 02:10:37 -0700

    fix container_of macro

--- lesson04/source/include/container_of.h
+++ lesson04/source/include/container_of.h
@@ -5,7 +5,7 @@
 #define offsetof(type,member) ((size_t)&(type *)0 -> member)
 
 #define container_of(ptr,type,member) ({ \
-                const typeof((type *)0 -> member) *__mptr = (ptr); \
+                const typeof(((type *)0) -> member) *__mptr = (ptr); \
                 (type *) ((char*)__mptr - offsetof(type, member)); \
                 })
 
