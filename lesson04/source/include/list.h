b12f7061d32ccd901598e76ef32f2f14d5e7448b
Zhao233 <953071759@qq.com>
2022-06-13 09:32:34 -0700

    add conditianal compilation to specify C declarations and assembly declarations

--- lesson04/source/include/list.h
+++ lesson04/source/include/list.h
@@ -1,4 +1,5 @@
 #include "container_of.h"
+#include "_stddef.h"
 
 #ifndef _LIST_H
 
@@ -9,9 +10,18 @@
 #define LIST_HEAD(name) \
         struct list_head name = LIST_HEAD_INIT(name)
 
-struct list_head{
+
+#define list_for_each(pos, head) \
+        for((pos) = (head)->next; pos != head; pos = pos -> next)
+
+#define list_entry(ptr, type, member) \
+        container_of(ptr, type, member)
+
+#ifndef __ASSEMBLY_DEF
+
+struct list_head {
     struct list_head *prev, *next;
-}
+};
 
 static inline void __list_add(struct list_head *prev, struct list_head *new, struct list_head *next){
     prev -> next = new;
@@ -35,5 +45,5 @@ static inline void list_del(struct list_head *head){
     head -> prev = NULL;
     head -> next = NULL;
 }
-
+#endif
 #endif
